2149e562cafaea5b0cc10d7483570f8447e4b1a4
venuswhispers <venuswhisperer412@gmail.com>
2019-12-03 17:34:07 +0100

    Replace frontend with @material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,235 +1,229 @@
 import React, { Component } from "react";
-import getWeb3, { getGanacheWeb3, Web3 } from "./utils/getWeb3";
-import Header from "./components/Header/index.js";
-import Footer from "./components/Footer/index.js";
-import Hero from "./components/Hero/index.js";
-import Web3Info from "./components/Web3Info/index.js";
+import { Button, Typography, Grid, TextField } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
 
-import { Loader, Button, Card, Input, Heading, Table, Form, Flex, Box, Image } from 'rimble-ui';
-import { zeppelinSolidityHotLoaderOptions } from '../config/webpack';
+// Import json file for artifact
+import TicketRegistry from "./contracts/TicketRegistry.json";
 
-import styles from './App.module.scss';
-//import './App.css';
+
+import getWeb3 from "./utils/getWeb3";
+
+import { theme } from './utils/theme';
+import Header from './components/Header';
+
+import "./App.css";
+
+const GAS = 500000;
+const GAS_PRICE = "20000000000";
 
 
 class App extends Component {
-  constructor(props) {    
-    super(props);
+    constructor(props) {    
+        super(props);
 
-    this.state = {
-      /////// Default state
-      storageValue: 0,
-      web3: null,
-      accounts: null,
-      route: window.location.pathname.replace("/", ""),
+        this.state = { 
+            web3: null, 
+            accounts: null,
+
+            ticket_registry: null        
+        };
+    }
+
+    componentDidMount = async () => {
+        try {
+            // Get network provider and web3 instance.
+            const web3 = await getWeb3();
+
+            // Use web3 to get the user's accounts.
+            const accounts = await web3.eth.getAccounts();
+
+            // Get the contract instance.
+            const networkId = await web3.eth.net.getId();
+            if (networkId !== 3) {
+                throw new Error("Select the Ropsten network from your MetaMask plugin");
+            }
+
+            const deployedNetworkTicketRegistry = TicketRegistry.networks[networkId];
+            const ticket_registry = new web3.eth.Contract(
+                TicketRegistry.abi,
+                deployedNetworkTicketRegistry && deployedNetworkTicketRegistry.address,
+            );
+
+            this.setState({ 
+              web3, 
+              accounts, 
+              ticket_registry: ticket_registry,
+            });
+
+            window.ethereum.on('accountsChanged', async (accounts) => {
+                const newAccounts = await web3.eth.getAccounts();
+                this.setState({ accounts: newAccounts });
+            });
+
+            // Refresh on-chain data every 1 second
+            const component = this;
+            async function loopRefresh() {
+                await component.refreshState();
+                setTimeout(loopRefresh, 1000);
+            }
+            loopRefresh();
+
+        } catch (error) {
+            // Catch any errors for any of the above operations.
+            alert(
+                `Failed to load web3, accounts, or contract. Check console for details.`,
+            );
+            console.error(error);
+        }
     };
 
-    this.getTestData = this.getTestData.bind(this);
-  }
 
+    /***********************************************************************
+     * WalletConnect Ticket dApp
+     ***********************************************************************/
+    refreshState = async () => {
+        const { accounts, ticket_registry } = this.state;
 
-
-  ///////--------------------- Functions of testFunc ---------------------------  
-  getTestData = async () => {
-    const { accounts, ticket_registry, web3 } = this.state;
-
-    const response_1 = await ticket_registry.methods.testFunc().send({ from: accounts[0] })
-    console.log('=== response of testFunc() function ===', response_1);
-  }
-
-
-
-  //////////////////////////////////// 
-  ///// Ganache
-  ////////////////////////////////////
-  getGanacheAddresses = async () => {
-    if (!this.ganacheProvider) {
-      this.ganacheProvider = getGanacheWeb3();
-    }
-    if (this.ganacheProvider) {
-      return await this.ganacheProvider.eth.getAccounts();
-    }
-    return [];
-  }
-
-  componentDidMount = async () => {
-    const hotLoaderDisabled = zeppelinSolidityHotLoaderOptions.disabled;
- 
-    let TicketRegistry = {};
-    try {
-      TicketRegistry = require("../../build/contracts/TicketRegistry.json"); // Load ABI of contract of TicketRegistry
-    } catch (e) {
-      console.log(e);
+        this.setState({});
     }
 
-    try {
-      const isProd = process.env.NODE_ENV === 'production';
-      if (!isProd) {
-        // Get network provider and web3 instance.
-        const web3 = await getWeb3();
-        let ganacheAccounts = [];
+    handleUpdateFundForm = (name, value) => {
+        this.setState({ [name]: value });
+    }
 
+    handleFund = async (fundResultString) => {
+        const { accounts, ticket_registry } = this.state;
+    }
+
+    handleTestFunc = async () => {
+        const { accounts, ticket_registry } = this.state;
         try {
-          ganacheAccounts = await this.getGanacheAddresses();
-        } catch (e) {
-          console.log('Ganache is not running');
+            //let walletAddr = accounts[0];
+            //let ipAddress = "185.199.104.14";
+            const response = await ticket_registry.methods.testFunc().send({ from: accounts[0] });
+            console.log("=== testFunc() ===", response)
+
+            await this.setState({ message: "Success to create beneficiary" });
         }
+        catch (error) {
+            console.error(error);
+            this.setState({ message: "Failed withdrawing" });
+        }
+    }
 
-        // Use web3 to get the user's accounts.
-        const accounts = await web3.eth.getAccounts();
-        // Get the contract instance.
-        const networkId = await web3.eth.net.getId();
-        const networkType = await web3.eth.net.getNetworkType();
-        const isMetaMask = web3.currentProvider.isMetaMask;
-        let balance = accounts.length > 0 ? await web3.eth.getBalance(accounts[0]): web3.utils.toWei('0');
-        balance = web3.utils.fromWei(balance, 'ether');
-
-        let instanceTicketRegistry = null;
-        let deployedNetwork = null;
-
-        // Create instance of contracts
-        if (TicketRegistry.networks) {
-          deployedNetwork = TicketRegistry.networks[networkId.toString()];
-          if (deployedNetwork) {
-            instanceTicketRegistry = new web3.eth.Contract(
-              TicketRegistry.abi,
-              deployedNetwork && deployedNetwork.address,
+    render() {
+        if (!this.state.web3) {
+            return (
+                <ThemeProvider theme={theme}>
+                    <div className="App">
+                        <Header />
+                        <Typography>
+                            Loading Web3, accounts, and contract...
+                        </Typography>
+                    </div>
+                </ThemeProvider>
             );
-            console.log('=== instanceTicketRegistry ===', instanceTicketRegistry);
-          }
         }
+        return (
+            <ThemeProvider theme={theme}>
+                <div className="App">
+                    <Header />
+                    <Typography variant="h5" style={{ marginTop: 32 }}>
+                        Ticket Registry
+                    </Typography>
+                    <Typography variant="h5" style={{ marginTop: 32 }}>
+                        {this.state.resultMessage}
+                    </Typography>
 
-        if (TicketRegistry) {
-          // Set web3, accounts, and contract to the state, and then proceed with an
-          // example of interacting with the contract's methods.
-          this.setState({ 
-            web3, 
-            ganacheAccounts, 
-            accounts, 
-            balance, 
-            networkId, 
-            networkType, 
-            hotLoaderDisabled,
-            isMetaMask, 
-            ticket_registry: instanceTicketRegistry,
-          }, () => {
-            this.refreshValues(
-              instanceTicketRegistry,
-            );
-            setInterval(() => {
-              this.refreshValues(instanceTicketRegistry);
-            }, 5000);
-          });
-        }
-        else {
-          this.setState({ web3, ganacheAccounts, accounts, balance, networkId, networkType, hotLoaderDisabled, isMetaMask });
-        }
-      }
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
+
+                    <Grid container style={{ marginTop: 32 }}>
+                        <Grid item xs={6}>
+                            <Typography variant="h5">
+                                Test
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Typography variant="h5">
+                                Text
+                            </Typography>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container style={{ marginTop: 32 }}>
+                        <Grid item xs={6}>
+                            <Typography variant="h5">
+                                {"Text"}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Typography variant="h5">
+                                {"test"}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container>
+                        <Grid item xs={6}>
+                            <Typography variant="h5">
+                                {"Text"}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Typography variant="h5">
+                                {"test"}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container style={{ marginTop: 32 }}>
+                        <Grid item xs={6}>
+                            <Typography variant="h5">
+                                {"Text"}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                id="bet-amount"
+                                className="input"
+                                value={this.state.fundAmount}
+                                onChange={e => this.handleUpdateFundForm('fundAmount', e.target.value)}
+                            />
+                        </Grid>
+                    </Grid>
+
+                    <Grid container style={{ marginTop: 32 }}>
+                        <Grid item xs={6}>
+                           test
+                        </Grid>
+                        <Grid item xs={1}>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Button variant="contained" color="primary" onClick={() => this.handleFund("true")}>
+                               Button 1
+                            </Button>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container style={{ marginTop: 32 }}>
+                        <Grid item xs={6}>
+                            test
+                        </Grid>
+                        <Grid item xs={1}>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Button variant="contained" color="primary" onClick={() => this.handleTestFunc()}>
+                                Test Func
+                            </Button>
+                        </Grid>
+                    </Grid>
+
+                    <Typography variant="h5" style={{ marginTop: 32 }}>
+                        {this.state.message}
+                    </Typography>
+                </div>
+            </ThemeProvider>
+        );
     }
-  };
-
-  componentWillUnmount() {
-    if (this.interval) {
-      clearInterval(this.interval);
-    }
-  }
-
-  refreshValues = (instanceTicketRegistry) => {
-    if (instanceTicketRegistry) {
-      console.log('refreshValues of instanceTicketRegistry');
-    }
-  }
-
-  renderLoader() {
-    return (
-      <div className={styles.loader}>
-        <Loader size="80px" color="red" />
-        <h3> Loading Web3, accounts, and contract...</h3>
-        <p> Unlock your metamask </p>
-      </div>
-    );
-  }
-
-  renderDeployCheck(instructionsKey) {
-    return (
-      <div className={styles.setup}>
-        <div className={styles.notice}>
-          Your <b> contracts are not deployed</b> in this network. Two potential reasons: <br />
-          <p>
-            Maybe you are in the wrong network? Point Metamask to localhost.<br />
-            You contract is not deployed. Follow the instructions below.
-          </p>
-        </div>
-      </div>
-    );
-  }
-
-  renderInstructions() {
-    return (
-      <div className={styles.wrapper}>
-        <Hero />
-      </div>
-    );
-  }
-
-  renderTrustlessPayment() {
-    const { ticket_registry } = this.state;
-
-    return (
-      <div className={styles.wrapper}>
-      {!this.state.web3 && this.renderLoader()}
-      {this.state.web3 && 
-        !this.state.ticket_registry &&
-        this.renderDeployCheck('ticket_registry')
-      }
-      {this.state.web3 && 
-       this.state.ticket_registry && (
-        <div className={styles.contracts}>
-
-          <h2>Ticket Registry</h2>
-
-            <div className={styles.widgets}>
-              <Card width={'30%'} bg="primary">
-
-                <h4>Ticket Registry / Create Account</h4>
-
-                <Image
-                  alt="random unsplash image"
-                  borderRadius={8}
-                  height="100%"
-                  maxWidth='100%'
-                />
-
-                <span style={{ padding: "20px" }}></span>
-
-                <br />
-
-                <Button size={'small'} onClick={this.getTestData}> Ticket Registry </Button>
-              </Card>
-            </div>
-
-        </div>
-      )}
-      </div>
-    );
-  }
-
-  render() {
-    return (
-      <div className={styles.App}>
-        <Header />
-          {this.state.route === '' && this.renderInstructions()}
-          {this.state.route === 'trustless_payment' && this.renderTrustlessPayment()}
-        <Footer />
-      </div>
-    );
-  }
 }
 
 export default App;
